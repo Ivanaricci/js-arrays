97ce42a81fe4d4d3be0ffee83f8e53c533dd2fdd
Ivanaricci <ivana.ricci26@gmail.com>
2025-03-19 17:34:13 +0100

    [UPD] Es4-arrays-bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -35,7 +35,24 @@ console.log(teachers)
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+const isFabioPresent = prompt('inserisci il testo');
+let flag = false
+
+for(let i=0; i<teachers.length; i++){
+  if(isFabioPresent == teachers[i]){
+    flag = true
+  }
+}
+
+if(flag==true){
+  console.log('Elemento trovato')
+}
+
+else{
+  console.log('elemento non trovato')
+}
+
+
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = null;
